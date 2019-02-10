7b2757a0d25bc4a6089ed30b0b901361844aaa18
Pradeep Reddy Raamana <raamana@gmail.com>
2019-02-10 14:05:52 -0500

    tests for updated methods for check buttons

--- lib/matplotlib/tests/test_widgets.py
+++ lib/matplotlib/tests/test_widgets.py
@@ -253,7 +253,8 @@ def test_lasso_selector():
 
 def test_CheckButtons():
     ax = get_ax()
-    check = widgets.CheckButtons(ax, ('a', 'b', 'c'), (True, False, True))
+    labels = ('a', 'b', 'c')
+    check = widgets.CheckButtons(ax, labels, (True, False, True))
     assert check.get_status() == [True, False, True]
     check.set_active(0)
     assert check.get_status() == [False, False, True]
@@ -261,6 +262,14 @@ def test_CheckButtons():
     check.clear()
     assert not any(check.get_status())
     assert len(check.get_checked_labels()) == 0
+
+    for invalid_index in [-1, len(labels), len(labels)+5]:
+        with pytest.raises(ValueError):
+            check.set_active(index=invalid_index)
+
+    for invalid_value in ['invalid', -1, None]:
+        with pytest.raises(TypeError):
+            check.set_active(1, state=invalid_value)
 
     cid = check.on_clicked(lambda: None)
     check.disconnect(cid)
